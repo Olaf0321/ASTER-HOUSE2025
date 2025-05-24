536b925cf0c431a088ab0cd168f4c6fe0d60024c
Olaf0321 <daikifujimoto344@gmail.com>
2025-05-24 15:46:28 +0900

    [FIX]: re-update 1 folder - update Project as new SP templete

--- html_asterhouseLP202505/php/config-2.php
+++ html_asterhouseLP202505/php/config-2.php
@@ -7,7 +7,34 @@
 $reply_mail = 1;
 
 
+// 送信データの取得＋簡易サニタイズ
+$type = isset($_POST['inquiry']) ? trim($_POST['inquiry']) : '';
+$name     = isset($_POST['name'])   ? trim($_POST['name'])   : '';
+$furigana = isset($_POST['furigana']) ? trim($_POST['furigana']) : '';
+$mail     = isset($_POST['mail_address_confirm'])     ? trim($_POST['mail_address_confirm'])     : '';
+$address  = isset($_POST['address'])  ? trim($_POST['address'])  : '';
+$phone    = isset($_POST['phone'])    ? trim($_POST['phone'])    : '';
+$message  = isset($_POST['message'])  ? trim($_POST['message'])  : '';
 
+
+$type_labels = [
+    'general'      => '資料請求',
+    'consultation' => '家づくり相談会のご予約',
+    'other'        => 'その他、住まいに関するご相談',
+];
+// 実際にメールに出力するラベル
+$type_label = isset($type_labels[$type])
+    ? $type_labels[$type]
+    : '未選択';
+
+
+// リモートホスト名
+$remote_host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+
+// 必要に応じて JavaScript チェックやリファラも取得
+$javascript_comment = $_POST['javascript_comment'] ?? '';
+$now_url            = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$before_url         = $_SERVER['HTTP_REFERER'] ?? '';
 
 //自分に届くメールの内容 -- EOMからEOM;までの間の文章を自分に合わせて変更してください。 --
 $send_body = <<<EOM
@@ -17,9 +44,10 @@ $send_body = <<<EOM
 
 --------------------------------------------------------------------------
 
+お問い合わせの種類：{$type_label}
 名前：{$name}
 フリガナ：{$furigana}
-メールアドレス：{$mail}
+メールアドレス：{$mail_address}
 ご住所：{$address}
 電話番号：{$phone}
 お問い合わせの内容：
@@ -33,7 +61,6 @@ $send_body = <<<EOM
 
 送信前の入力チェック：{$javascript_comment}
 メールフォームのURL：{$now_url}
-メールフォームに付く前のURL：{$before_url}
 
 EOM;
 
@@ -51,9 +78,10 @@ $thanks_body = <<<EOM
 
 --------------------------------------------------------------------------
 
+お問い合わせの種類：{$type_label}
 名前：{$name}
 フリガナ：{$furigana}
-メールアドレス：{$mail}
+メールアドレス：{$mail_address_confirm}
 ご住所：{$address}
 電話番号：{$phone}
 お問い合わせの内容：
